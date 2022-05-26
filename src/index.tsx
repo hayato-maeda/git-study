9fca9a3cbd161dcaa78bb6833554163df0925e3b
hayato-maeda <hayato.minto1011@gmail.com>
2022-05-26 21:47:02 +0900

    useForm勉強用に整備

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,61 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import Select from "react-select";
+import { createRoot } from 'react-dom/client';
+import { useForm, Controller } from "react-hook-form";
+import { Input } from '@mui/material';
+import { Input as AntdInput } from "antd";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./styles.css";
+
+interface IFormInput {
+  firstName: string;
+  lastName: string;
+  iceCreamType: { label: string; value: string };
+}
+
+const App = () => {
+  const { control, handleSubmit } = useForm<IFormInput>();
+
+  const onSubmit = (data: IFormInput) => {
+    alert(JSON.stringify(data));
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>First Name</label>
+      <Controller
+        render={({ field }) => <Input {...field} className="materialUIInput" />}
+        name="firstName"
+        control={control}
+        defaultValue=""
+      />
+      <label>First Name</label>
+      <Controller
+        render={({ field }) => <AntdInput {...field} />}
+        name="lastName"
+        control={control}
+        defaultValue=""
+      />
+      <label>Ice Cream Preference</label>
+      <Controller
+        name="iceCreamType"
+        render={({ field }) => (
+          <Select
+            {...field}
+            options={[
+              { value: "chocolate", label: "Chocolate" },
+              { value: "strawberry", label: "Strawberry" },
+              { value: "vanilla", label: "Vanilla" }
+            ]}
+          />
+        )}
+        control={control}
+      />
+
+      <input type="submit" />
+    </form>
+  );
+};
+
+const container = document.getElementById('root');
+ const root = createRoot(container!);
+ root.render(<App />);
