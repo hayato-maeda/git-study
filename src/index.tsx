b4db8d4c8f91cecad2eb81620af45081a72ba2bc
hayato-maeda <hayato.minto1011@gmail.com>
2022-05-28 12:03:56 +0900

    udemt-React

--- src/index.tsx
+++ src/index.tsx
@@ -1,64 +1,13 @@
-import Select from "react-select";
-import { createRoot } from 'react-dom/client';
-import { useForm, Controller } from "react-hook-form";
-import { Input } from '@mui/material';
-import { Input as AntdInput } from "antd";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-import "./styles.css";
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-interface IFormInput {
-  firstName: string;
-  lastName: string;
-  iceCreamType: { label: string; value: string };
-}
 
-const App = () => {
-  const { control, handleSubmit } = useForm<IFormInput>();
-
-  const onSubmit = (data: IFormInput) => {
-    console.log(data);
-  };
-
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label>First Name</label>
-      <Controller
-        render={({ field }) => <Input {...field} className="materialUIInput" />}
-        name="firstName"
-        control={control}
-        defaultValue="hayato"
-      />
-      <label>First Name</label>
-      <Controller
-        render={({ field }) => <AntdInput {...field} />}
-        name="lastName"
-        control={control}
-        defaultValue="maeda"
-      />
-      <label>Ice Cream Preference</label>
-      <Controller
-        control={control}
-        name="iceCreamType"
-        render={({ field }) => (
-          <Select
-            onChange={field.onChange}
-            onBlur={field.onBlur}
-            name={field.name}
-            value={field.value}
-            ref={field.ref} 
-            options={[
-              { value: "chocolate", label: "Chocolate" },
-              { value: "strawberry", label: "Strawberry" },
-              { value: "vanilla", label: "Vanilla" }
-            ]}
-          />
-        )}
-      />
-      <input type="submit" />
-    </form>
-  );
-};
-
-const container = document.getElementById('root');
- const root = createRoot(container!);
- root.render(<App />);
