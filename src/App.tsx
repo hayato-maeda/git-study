25e8477d4ec612c201366b02c77259860d838764
hayato-maeda <hayato.minto1011@gmail.com>
2022-05-29 15:29:35 +0900

    ExcelUpload

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
 
 const App: React.FC = () => {
-  const todos = [{ id: "t1", text: "TypeScriptコースの完了" }];
+  const todos = [{ id: "t1", text: "TypeScriptコースの完了!" }];
   return (
     <div className="App">
       <NewTodo />
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,19 +1,60 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+// import * as XLSX from "xlsx";
 
 const NewTodo: React.FC = () => {
-  const textInputRef = useRef<HTMLInputElement>(null);
-  const todoSubmitHandler = (event: React.FormEvent) => {
-    event.preventDefault();
-    const enteredText = textInputRef.current!.value;
-    console.log(enteredText);
+  const fileInput = useRef<HTMLInputElement>(null);
+  const [fileName, setFileName] = useState("");
+  const [excelData, setExcelData] = useState("");
+
+  const handleReadFile = (fileObj: File) => {
+    // console.log(fileObj);
+    const reader = new FileReader();
+    reader.readAsDataURL(fileObj);
+    reader.onload = () => {
+      const val = reader.result;
+      console.log(val);
+    }
+    console.log(fileObj);
+    if (fileObj) {
+      setFileName(fileObj.name);
+      fileObj.arrayBuffer().then((buffer) => {
+        // const workbook = XLSX.read(buffer, { type: "buffer", bookVBA: true });
+        // const firstSheetName = workbook.SheetNames[0];
+        // const worksheet = workbook.Sheets[firstSheetName];
+        // const data = XLSX.utils.sheet_to_json(worksheet);
+        // setExcelData(JSON.stringify(data));
+      });
+    }
   };
+
   return (
-    <form onSubmit={todoSubmitHandler}>
+    // <form onSubmit={todoSubmitHandler}>
+    <form>
       <div>
+        {!!fileName && <span>ファイル名：{fileName}</span>}
         <label htmlFor="todo-text">Todo内容</label>
-        <input type="file" id="todo-text" ref={textInputRef} />
+        <input
+          type="file"
+          id="todo-text"
+          ref={fileInput}
+          onChange={(e) => {
+            e.preventDefault();
+            handleReadFile(e.currentTarget.files![0]);
+          }}
+        />
       </div>
       <button type="submit">TODO追加</button>
+      {!!excelData && (
+        <div
+          style={{
+            border: "solid 1px #444",
+            marginTop: "10px",
+            padding: "10px",
+          }}
+        >
+          {excelData}
+        </div>
+      )}
     </form>
   );
 };
